ad4879f555bafb2a4feeb6da9bd65cdc4f1c3db5
Dominic Tarr <dominic.tarr@gmail.com>
2013-07-02 16:31:53 -0700

    versions commands

---
+++ plugins/versions.js
@@ -0,0 +1,17 @@
+
+
+exports.cli = function (db, config) {
+  db.commands.push(function (db, config, cb) {
+    var args = config._.slice()
+    if(args.shift() != 'versions') return
+    var module = args.shift()
+    db.sublevel('ver').createReadStream({
+      min: module + '!', max: module + '!~'
+    }).on('data', function (data) {
+      var pkg = data.value
+      console.log(pkg.name + '@' + pkg.version)
+    })
+    .on('end', cb)
+    return true
+  })
+}
