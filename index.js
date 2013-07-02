bced6b3aa02c5a5e09ffac2401cc0fbbdb3fc321
Dominic Tarr <dominic.tarr@gmail.com>
2013-07-01 22:14:46 -0700

    change plugin pattern

--- index.js
+++ index.js
@@ -8,105 +8,102 @@ var multilevel = require('multilevel')
 var levelup    = require('level')
 var sublevel   = require('level-sublevel')
 var Manifest   = require('level-manifest')
+var npmconf    = require('npmconf')
 
-var config     = require('./config')
+var getConf    = require('./config')
+var commands   = require('./options')
+
 var manifest   = require('./manifest.json')
-var config     = require('./config')
 
-var db
+//pull npmconf, so know where to install global modules.
+npmconf.load({}, function (err, conf) {
+  var config = getConf(conf)
 
-function createDb (db) {
-  if(!db) db = sublevel(levelup(config.path, config))
-  return db
-}
+  var db
 
-var plugins = [
-  require('./plugins/couch-sync'),
-  require('./plugins/inverted-index'),
-  require('./plugins/authors'),
-  require('npmd-resolve'),
-  require('npmd-install')
-]
-
-function addCommands(db) {
-  db.commands = {}
-  plugins.forEach(function (e) {
-    if(e.commands)
-      e.commands(db)
-  })
-}
-
-function addDb (db, config) {
-  db.config = config
-  plugins.forEach(function (e) {
-    if('function' === typeof e.db)
-      e.db(db, config)
-    else if('function' === typeof e)
-      e(db, config)
-  })
-}
-
-//TODO: make a middleware like thing but with
-//cli commands.
-
-function execCommands (db, config, cb) {
-  var called = false
-  if(!config._.length)
-    return cb(null, false)
-
-  var command = config._.shift()
-  if(db.commands[command]) {
-    called = true
-    return db.commands[command](config, cb)
+  function createDb (db) {
+    if(!db) db = sublevel(levelup(config.path, config))
+    return db
   }
-  cb(null, false)
-}
 
-function printHelp () {
-  fs.createReadStream(__dirname + '/README.md')
-  .on('close', function () {
-    process.exit()
-  })
-  .pipe(process.stdout)
-}
+  var plugins = [
+    require('./plugins/couch-sync'),
+    require('./plugins/inverted-index'),
+    require('./plugins/authors'),
+    require('npmd-resolve'),
+    require('npmd-install')(config),
+    {commands: function (db) {
+      db.commands.push(function (db, config, cb) {
+          fs.createReadStream(__dirname + '/README.md')
+          .on('close', function () {
+            cb()
+          })
+          .pipe(process.stdout)
+        return true
+        })
+      }
+    }
+  ]
 
-server = autonode(function (stream) {
-  var dbStream = this.isServer
-    ? multilevel.server(db)
-    : multilevel.client(manifest)
-
-  stream.pipe(dbStream).pipe(stream)
-  stream.on('error', function () {console.error('disconnected')})
-  
-  if(this.isClient) {
-    //process commands.
-    addCommands(dbStream)
-    execCommands(dbStream, config, function (err, did) {
-      if(err) throw err
-      server.close(); stream.end()
-      if(did === false)
-        printHelp()
+  function addCommands(db) {
+    db.cli = db.commands = []
+    plugins.forEach(function (plug) {
+      var cli = plug.cli || plug.commands
+      if('function' === typeof cli)
+        cli(db, config)
     })
   }
-})
-.listen(config.port)
-.on('listening', function () {
-  db = createDb()
-  //attach all plugins.
-  //process any commands.
-  addDb(db, config)
-  addCommands(db, config)
 
-  var manifest = Manifest(db, true)
+  function addDb (db, config) {
+    db.config = config
+    plugins.forEach(function (e) {
+      if('function' === typeof e.db)
+        e.db(db, config)
+    })
+  }
 
-  fs.writeFileSync(
-    __dirname+'/manifest.json',
-    JSON.stringify(manifest, null, 2)
-  )
-  execCommands(db, config, function (err, data) {
-    if(err) throw err
-    if(!config.sync)
-      printHelp(data)
+  //TODO: make a middleware like thing but with
+  //cli commands.
+
+  function execCommands (db, config, cb) {
+    if(!commands(db.commands) (db, config, cb))
+      cb(new Error('unknown command'))
+  }
+
+
+  server = autonode(function (stream) {
+    var dbStream = this.isServer
+      ? multilevel.server(db)
+      : multilevel.client(manifest)
+
+    stream.pipe(dbStream).pipe(stream)
+    stream.on('error', function () {console.error('disconnected')})
+  
+    if(this.isClient) {
+      //process commands.
+      addCommands(dbStream)
+      execCommands(dbStream, config, function (err, did) {
+        server.close(); stream.end()
+      })
+    }
+  })
+  .listen(config.port)
+  .on('listening', function () {
+    db = createDb()
+    //attach all plugins.
+    //process any commands.
+    addDb(db, config)
+    addCommands(db, config)
+
+    var manifest = Manifest(db, true)
+
+    fs.writeFileSync(
+      __dirname+'/manifest.json',
+      JSON.stringify(manifest, null, 2)
+    )
+    execCommands(db, config, function (err, data) {
+      if(err) throw err
+      process.exit()
+    })
   })
 })
-
