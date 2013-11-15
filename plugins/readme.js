9d64e47efbf3aa80b869d66c2f6dcc3f6a770ed9
fraserxu <xvfeng123@gmail.com>
2013-11-16 00:50:16 +0800

    fixed a typo

--- plugins/readme.js
+++ plugins/readme.js
@@ -1,9 +1,8 @@
-
 exports.cli = function (db, config) {
   db.commands.push(function (db, config, cb) {
     var args = config._.slice()
     if(args.shift() != 'readme') return
-    var modlue = args.shift()
+    var module = args.shift()
     db.sublevel('pkg').createReadStream({
       min: module, max: module
     }).on('data', function (data) {
