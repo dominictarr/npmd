8005ba5c0b4a07c35db06d7636afc863bb179238
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-17 14:40:00 +0800

    weight search results by module name, description, readme

--- plugins/inverted-index.js
+++ plugins/inverted-index.js
@@ -3,7 +3,7 @@ var Inverted = require('level-inverted-index')
 var through  = require('through')
 var strftime = require('strftime')
 var context = require('search-context')
-
+var sort = require('sort-stream')
 function rpad (s, n) {
   if (!s) s = ''
   return s + Array(Math.max(2, n - s.length + 1)).join(' ')
@@ -28,6 +28,7 @@ exports.db = function (db, config) {
     index(value.keywords)
     index(value.description)
   }, function (value, query) {
+    var stats = context.stats(value.readme || value.description || '', query)
     return {
       name: value.name,
       maintainers: value.maintainers,
@@ -35,19 +36,48 @@ exports.db = function (db, config) {
       maintainers: value.maintainers,
       time: strftime('%F %H:%M', new Date(value.time && value.time.modified)),
       keywords: value.keywords || [],
-      readme: value.readme
+      readme: value.readme,
+      stats: stats
     }
   })
 }
 
 exports.commands = function (db) {
+
+  function presense (string, args) {
+    return Math.pow(args.reduce(function (t, str) {
+      return t - (~string.indexOf(str) ? 1 : 0)
+    }, args.length), 2)
+  }
+
+  function rank (doc, query, c) {
+    return (
+      presense(doc.name, query)        * (c.searchWeightName        || 0.5)
+    + presense(doc.description, query) * (c.searchWeightDescription || 2)
+    + (
+        (doc.stats.stddev              * (c.searchWeightStddev      || 1))
+      * (doc.stats.avg                 * (c.searchWeightAvg         || 0.2))
+      )                                * (c.searchWeightGroup       || 1)
+    )
+  }
+
   db.commands.push(function (db, config, cb) {
+
   var args = config._.slice()
   if(args.shift() != 'search')
     return
 
   if(!args.length)
     return cb(new Error('expects search term'))
+
+  function highlight (string) {
+    return context.highlight(
+      string, args,
+      function (e) {
+        return process.stdout.isTTY ? e.bold : '*' + e +'*'
+      }
+    )
+  }
 
   var header = rpad('NAME', 22)
     + rpad('DESCRIPTION', 61)
@@ -58,6 +88,15 @@ exports.commands = function (db) {
  
   db.sublevel('index')
     .createQueryStream(args, {})
+    .pipe(through(function (e) {
+      e.value.stats = context.stats(e.value.readme || e.value.description || '', args) || {}
+      e.value.rank = rank(e.value, args, config)
+      this.queue(e)
+    }))
+    .pipe(sort(function (a, b) {
+      //XXX SORT
+      return a.value.rank - b.value.rank
+    }))
     .pipe(through(function (data) {
       var over = Math.max(0, data.key.length - 21)
       var authors = data.value.maintainers
@@ -68,7 +107,7 @@ exports.commands = function (db) {
         + trim(rpad(authors, 22), 22 - over)
         + trim(rpad(data.value.time, 18), 18 - over)
         + data.value.keywords.join(' ')
-      this.queue((process.stdout.isTTY
+      this.queue(highlight(process.stdout.isTTY
         ? line.slice(0, process.stdout.columns)
         : line
       ) + '\n')
@@ -79,13 +118,19 @@ exports.commands = function (db) {
         //this is really crude, at the moment.
   
         var l = (config.context || (process.stdout.isTTY ? process.stdout.columns - 10 : 70))
-        var m = context(data.value.readme, args, l)
-        return '    ...'+ (
+
+        var m = 
+          context.context(data.value.readme, data.value.stats.group, args, l)
+
+        return '    ...'+ highlight(
           m.split('\n')
           .map(function (e) { return e.trim() })
           .join(' ')
           .trim()
         ) + '...\n'
+        //option to show the search rank.
+        //useful when tuning search ranking
+        + (config['show-rank'] ? 'rank=' + data.value.rank + '\n' : '')
       })() || '')
 
     }))
