ad1cdcc31a1a4f3845afcee457d68acf36a7c230
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-17 17:12:25 +0800

    fix bug with undefined

--- plugins/packages.js
+++ plugins/packages.js
@@ -3,11 +3,12 @@ exports.cli = function (db, config) {
   db.commands.push(function (db, config, cb) {
     var args = config._.slice()
     if(args.shift() != 'packages') return
-    var module = args.shift()
+    var module = args.shift() || ''
     db.sublevel('pkg').createReadStream({
-      min: module , max: module + '~'
+      min: module , max: module + '~',
+      keys: false
     }).on('data', function (data) {
-      var pkg = data.value
+      var pkg = data
       if(config.long)
         console.log(JSON.stringify(pkg, null, 2))
       else
