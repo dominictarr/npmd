dbd285cd49598ca8047e3bf5435342752207c5a3
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-21 10:55:11 +0800

    opps, didn't commit plugins/tree

---
+++ plugins/tree.js
@@ -0,0 +1,28 @@
+var tree = require('npmd-tree')
+console.log(tree)
+
+exports.cli = function (db) {
+  db.commands.push(function (db, config, cb) {
+    var args = config._.slice()
+    var cmd = args.shift()
+  
+    if(cmd == 'tree') {
+      tree.tree(config.installPath, config, function (err, tree) {
+        if(err) throw err
+        console.log(JSON.stringify(tree, null, 2))
+        cb()
+      })
+    }
+    else if(cmd == 'ls')
+      tree.ls(config.installPath, function (err, tree) {
+        if(err) throw err
+        console.log(JSON.stringify(tree, null, 2))
+        cb()
+      })
+    else
+      return
+
+    return true
+  })
+}
+
