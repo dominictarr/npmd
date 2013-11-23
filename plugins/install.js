ea4f785abe7d19988bb21ea1a8b13620ae631f5a
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-23 11:44:30 +0800

    update install command to build, and to link modules

--- plugins/install.js
+++ plugins/install.js
@@ -1,4 +1,7 @@
 var deps = require('get-deps')
+var rebuild = require('../plugins/rebuild').rebuild
+var bin = require('npmd-bin')
+var path = require('path')
 
 exports.commands = function (db, config) {
 
@@ -7,9 +10,31 @@ exports.commands = function (db, config) {
     var args = config._.slice()
     if('install' !== args.shift()) return
 
-    db.resolve(args, config, function (err, tree) {
+    var _args = args.slice()
+
+    db.resolve(args.slice(), config, function (err, tree) {
       if(err) return cb(err)
-      install(tree, config, cb)
+      install(tree, config, function (err, installed) {
+        if(err) return cb(err)
+
+        rebuild(_args[0], config, function (err) {
+
+          _args = _args.map(function (e) {
+            e = e.split('@').shift()
+            if(/^[./]/.test(e)) return e
+            return config.global
+              ? path.join(config.prefix, 'lib', 'node_modules', e)
+              : path.join(process.cwd(), 'node_modules', e)
+          })
+
+          console.log(_args, config.bin)
+
+          bin.all(_args, config.bin, config, cb)
+
+        })
+      })
+
+      //and then build, and link deps
     })
     return true
   })
