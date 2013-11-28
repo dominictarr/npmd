af67220fbd895eda6b5e7d58c8afeb9584d0382f
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-28 23:26:56 +0800

    refactor out rebuild into it's own command

--- plugins/rebuild.js
+++ plugins/rebuild.js
@@ -1,31 +1,5 @@
 
-var tree = require('npmd-tree').tree
-var spawn = require('child_process').spawn
-var path = require('path')
-
-var rebuild = exports.rebuild = function (target, config, cb) {
-  config.post =
-    function (pkg, cb) {
-      if(!pkg.gypfile) return cb(null, pkg)
-      var cp =
-        spawn('node-gyp', ['rebuild'], {cwd: pkg.path})
-        .on('exit', function (code) {
-          cb(code === 0 ? null : new Error('node-gyp: non-zero exit'), pkg)
-        })
-        .on('error', cb)
-
-      cp.stdout.pipe(process.stdout)
-      cp.stderr.pipe(process.stderr)
-    }
-
-  target = target || config.path || process.cwd()
-
-  if(!/^[./]/.test(target))
-    target =
-      path.join(config.path || process.cwd(), 'node_modules', target)
-
-  tree(target, config, cb)
-}
+var rebuild = require('npmd-rebuild')
 
 exports.commands = function (db, config) {
 
